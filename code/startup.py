784ce01ed7e9b13fdd8371b920dfbc18852da256
niko-todorov <todorov@chapman.edu>
2023-02-08 22:19:52 -0800

    API key
    removing the Google API key for safety

--- code/startup.py
+++ code/startup.py
@@ -5,7 +5,7 @@ from brails.InventoryGenerator import InventoryGenerator
 # Initialize InventoryGenerator:
 invGenerator = InventoryGenerator(location='Orange, CA',
                                   nbldgs=100, randomSelection=True,
-                                  GoogleAPIKey="AIzaSyBOuFNQ82pIHrk5kyE6N5Yeg_028gDwc2Q")
+                                  GoogleAPIKey="")
 
 # Run InventoryGenerator to generate an inventory for the entered location:
 # To run InventoryGenerator for all enabled attributes set attributes='all':
