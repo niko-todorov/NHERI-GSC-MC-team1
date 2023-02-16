5e66e8938401bb48837aaeb2543a6b994711616d
niko-todorov <todorov@chapman.edu>
2023-02-15 23:04:36 -0800

    update inv gen
    need to be careful with the api key

--- code/startup.py
+++ code/startup.py
@@ -1,15 +1,19 @@
 import brails
 #import InventoryGenerator:
 from brails.InventoryGenerator import InventoryGenerator
-
+location='Coronita, CA' # 'Gaziantep, TR'
 # Initialize InventoryGenerator:
-invGenerator = InventoryGenerator(location='Orange, CA',
-                                  nbldgs=100, randomSelection=True,
-                                  GoogleAPIKey="")
+invGenerator = InventoryGenerator(
+  location=location, nbldgs=9, randomSelection=True,
+  GoogleAPIKey="")
 
 # Run InventoryGenerator to generate an inventory for the entered location:
 # To run InventoryGenerator for all enabled attributes set attributes='all':
-invGenerator.generate(attributes=['numstories','roofshape','buildingheight'])
+# 'buildingheight','chimney','erabuilt','garage','numstories','occupancy',
+# 'roofcover','roofeaveheight','roofshape','roofpitch'
+invGenerator.generate(attributes=
+['occupancy','erabuilt','garage','numstories','buildingheight'])
 
-# View generated inventory:
-invGenerator.inventory
+# Output generated inventory to ..\data\:
+df=invGenerator.inventory
+df.to_csv('../data/'+location+'.csv')
